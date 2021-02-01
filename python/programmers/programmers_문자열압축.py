7829a55d3dca17acdc9d3d2390d34c173fc20661
MyeongHwan <kmh03214@gmail.com>
2021-02-02 02:27:40 +0900

    programmers 문자열 압축

---
+++ python/programmers/programmers_문자열압축.py
@@ -0,0 +1,18 @@
+def compress(s,length,ret_s):
+    while 2*length <= len(s):
+        sub, k = s[:length], 1
+        while sub == s[k*length:(k+1)*length]:
+            k += 1
+        if k != 1:
+            ret_s += str(k)+sub
+            s = s[k*length:]
+        else:
+            ret_s += s[:length]
+            s = s[length:]
+    ret_s += s
+    return len(ret_s)
+
+def solution(s):
+    return min([compress(s,i,'') for i in range(1,(len(s)//2) +1)])
+
+solution('a')
