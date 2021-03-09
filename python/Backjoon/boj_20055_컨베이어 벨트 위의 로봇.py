6bfdde9d45e584026155a140a100a4db961838f2
MyeongHwan <kmh03214@gmail.com>
2021-03-09 19:52:12 +0900

    boj_20055 컨베이어벨트위의로봇python and pypy3

--- python/Backjoon/boj_20055_컨베이어 벨트 위의 로봇.py
+++ python/Backjoon/boj_20055_컨베이어 벨트 위의 로봇.py
@@ -1,1 +1,32 @@
-print([i for i in range(300)])
+import sys
+read = sys.stdin.readline
+N, K = map(int,read().split())
+
+belt = [[i,0] for i in map(int,read().split())] # (내구도,로봇여부)
+cnt,sol =0, 0
+def move_belt():
+    global belt
+    belt = [belt[-1]] + belt[:-1] # 회전
+    if belt[N-1][1] == 'R':
+        belt[N-1][1] = 0
+
+def move_robot():
+    global cnt
+    for i in range(N-2,-1,-1):
+        if belt[i][1] == 'R' and belt[i+1][1] != 'R' and belt[i+1][0] != 0:
+            belt[i+1][0], belt[i][1], belt[i+1][1] = belt[i+1][0]-1,0,'R'
+            if belt[i+1][0] == 0:
+                cnt += 1
+    if belt[N-1][1] == 'R':
+        belt[N-1][1] = 0
+    if belt[0][0] > 0:
+        belt[0][0], belt[0][1] = belt[0][0]-1, 'R'
+        if belt[0][0] == 0:
+            cnt += 1
+
+
+while cnt < K:
+    sol += 1
+    move_belt()
+    move_robot()
+print(sol)
--- python/Backjoon/boj_20055_컨베이어 벨트위의 로봇_pypy3.py
+++ python/Backjoon/boj_20055_컨베이어 벨트위의 로봇_pypy3.py
@@ -2,52 +2,26 @@ import sys
 read = sys.stdin.readline
 N, K = map(int,read().split())
 
-global belt, robot
-belt = list(map(int,read().split()))
-robot = [0 for i in range(N)]
-
-def robot_bye():
-    if robot[-1] == -1:
-        robot[-1] = 0
+belt = [[i,0] for i in map(int,read().split())] # (내구도,로봇여부)
 
 def move_belt():
-    global belt,robot
-    belt = [belt[-1]] + belt[:-1]
-    robot = [0] + robot[:-1]
-    robot_bye()
+    global belt
+    belt = [belt[-1]] + belt[:-1] # 회전
+    if belt[N-1][1] == 'R':
+        belt[N-1][1] = 0
 
 def move_robot():
-    global belt,robot
-    next_robot = [0 for i in range(N)]
-    for i in range(N-1,-1,-1):
-        if robot[i] == -1 and belt[i+1] > 0:
-            if next_robot[i+1] == -1:
-                next_robot[i] = -1
-                robot[i] = 0
-                continue
-            next_robot[i+1] = -1
-            robot[i+1] = -1
-            robot[i] = 0
-        elif robot[i] == -1 and belt[i+1] <= 0:
-            next_robot[i] = -1
-            robot[i] = 0
-
-    for i in range(N):
-        belt[i] += robot[i]
-
-    robot = next_robot
-    robot_bye()
-
-def robot_hi():
-    if belt[0] > 0:
-        robot[0] = -1
-        belt[0] -= 1
-step = 0
-while True:
-    step += 1
+    for i in range(N-2,-1,-1):
+        if belt[i][1] == 'R' and belt[i+1][1] != 'R' and belt[i+1][0] != 0:
+            belt[i+1][0], belt[i][1], belt[i+1][1] = belt[i+1][0]-1,0,'R'
+    if belt[N-1][1] == 'R':
+        belt[N-1][1] = 0
+    if belt[0][0] > 0:
+        belt[0][0], belt[0][1] = belt[0][0]-1, 'R'
+cnt,sol = 0,0
+while cnt < K:
+    sol += 1
     move_belt()
     move_robot()
-    robot_hi()
-    if belt.count(0) >= K:
-        print(step)
-        break
+    cnt = sum([1 for item in belt if item[0] == 0])
+print(sol)
