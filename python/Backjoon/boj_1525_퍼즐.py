a2a0a383a371f3b1f7be511453c96bae8ccbae9f
MyeongHwan <kmh03214@gmail.com>
2021-04-06 19:45:57 +0900

    boj_1525_퍼즐

--- python/Backjoon/boj_1525_퍼즐.py
+++ python/Backjoon/boj_1525_퍼즐.py
@@ -1,3 +1,41 @@
+import sys
+read = sys.stdin.readline
+destination = ''
+for i in range(3):
+    destination += ''.join(read().split())
+
+check = {'123456780':1}
+def change(string,pos1,pos2):
+    mem = string[pos1]
+    string = string.replace(string[pos1],string[pos2])
+    return string[:pos2] + mem + string[pos2+1:]
+
+def search(state,zero_pos):
+    q,cnt = [[state,zero_pos]], 0
+    while q:
+        Next = []
+        for st, zp in q:
+            for i in [-1,1,-3,3]:
+                if (zp == 2 or zp == 5) and i == 1:
+                    continue
+                if (zp == 3 or zp == 6) and i == -1:
+                    continue
+                nzp = zp + i
+                if 0 <= nzp < 9:
+                    nst = change(st,zp,nzp)
+                    if nst == destination:
+                        return cnt
+                    if nst not in check:
+                        Next.append([nst,nzp])
+                        check[nst] = 1
+        q = Next
+        cnt += 1
+    return -2
+if destination == '123456780':
+    print(0)
+else:
+    print(search('123456780',8)+1)
+
 # import sys
 # read = sys.stdin.readline
 # mat = []
@@ -45,41 +83,3 @@
 # else:
 #     print(-1)
 
-import sys
-read = sys.stdin.readline
-destination = ''
-for i in range(3):
-    destination += ''.join(read().split())
-
-check = {'123456780':1}
-def change(string,pos1,pos2):
-    mem = string[pos1]
-    string = string.replace(string[pos1],string[pos2])
-    return string[:pos2] + mem + string[pos2+1:]
-
-def search(state,zero_pos):
-    q,cnt = [[state,zero_pos]], 0
-    while q:
-        Next = []
-        for st, zp in q:
-            for i in [-1,1,-3,3]:
-                if (zp == 2 or zp == 5) and i == 1:
-                    continue
-                if (zp == 3 or zp == 6) and i == -1:
-                    continue
-                nzp = zp + i
-                if 0 <= nzp < 9:
-                    nst = change(st,zp,nzp)
-                    if nst == destination:
-                        return cnt
-                    if nst not in check:
-                        Next.append([nst,nzp])
-                        check[nst] = 1
-                    
-        q = Next
-        cnt += 1
-    return -2
-if destination == '123456780':
-    print(0)
-else:
-    print(search('123456780',8)+1)
