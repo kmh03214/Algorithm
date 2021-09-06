7140cd0018ffe10d3ae922694674bb605838aa9c
MyeongHwan <kmh03214@gmail.com>
2021-09-06 15:47:52 +0900

    boj_2839_설탕배달.py

--- python/Algorithm_concept/greedy/boj_2839_설탕배달.py
+++ python/Algorithm_concept/greedy/boj_2839_설탕배달.py
@@ -2,20 +2,39 @@ import sys
 read = sys.stdin.readline
 
 N = int(read())
-if N < 5 and N != 3:
-    print(-1)
-elif N < 5 and N == 3:
-    print(1)
-else:
-    q,r = N//5, N%5
-    if r == 3:
-        print(q+1)
 
-    elif r == 1 or r == 4:
+q,r = N//5, N%5
+if r == 3:
+    print(q+1)
+elif r == 1 or r == 4:
+    if q-1 >= 0:
         print( (q-1) + (r+5)//3)
-
-    elif r == 2:
-        print( (q-2) + (r+10)//3 )
-    
     else:
-        print(q)
+        print(-1)
+elif r == 2:
+    if q-2 >= 0:
+        print( (q-2) + (r+10)//3 )
+    else:
+        print(-1)
+else:
+    print(q)
+
+# 가장 빠른 풀이?
+# n = int(input())
+
+# a = n//5
+# b = n%5
+# c = 0
+
+# while a>=0:
+#     if b%3 == 0:
+#         c = b//3
+        
+#         break
+#     a -= 1
+#     b += 5
+        
+# if b%3 == 0:
+#     print(a+c)
+# else:
+#     print(-1)
