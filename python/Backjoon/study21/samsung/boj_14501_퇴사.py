c9a761fecf1d8a65f67349d7296feaae25c673d0
MyeongHwan <kmh03214@gmail.com>
2021-09-25 03:59:18 +0900

    boj_14501_퇴사.py

--- python/Backjoon/study21/samsung/boj_14501_퇴사.py
+++ python/Backjoon/study21/samsung/boj_14501_퇴사.py
@@ -3,21 +3,18 @@ read = sys.stdin.readline
 N = int(read())
 TP = [list(map(int,read().split())) for i in range(N)]
 
-# Ti,Pi
-DP_table = {}
+# 정의 dp(i) -> i일 이후로 상담가능한 최대값
+# 상담한다 or not
+dp_table = {}
 def dp(i):
-    if i > N-1 or i + TP[i][0] > N:
+    if i > N-1:
         return 0
     
-    if i not in DP_table:
-        DP_table[i] = max(dp(i+TP[i][0])+TP[i][1], dp(i+1))
-        return DP_table[i]
+    if i not in dp_table:
+        dp_table[i] = max( [ dp(i+TP[i][0]) + TP[i][1] for j in range(1) if i+TP[i][0] <= N ] + [dp(i+1)] )
+        return dp_table[i]
     else:
-        return DP_table[i]
+        return dp_table[i]
 
-dp(0)
-if 0 in DP_table:
-    print(DP_table[0])
-else:
-    print(TP[0][1])
+print(dp(0))
 
