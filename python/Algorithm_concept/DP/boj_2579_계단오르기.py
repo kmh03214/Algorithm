0d26e56288b81f6e73ad2e9414819af2512fa8aa
MyeongHwan <kmh03214@gmail.com>
2021-10-05 21:59:58 +0900

    boj_2579_계단오르기.py

---
+++ python/Algorithm_concept/DP/boj_2579_계단오르기.py
@@ -0,0 +1,17 @@
+import sys
+read = sys.stdin.readline
+N = int(read())
+S = [0] + [int(read()) for i in range(N)]
+dp_table = {}
+def dp(stair,cnt):
+    if stair > N or cnt >= 3:
+        return -100000000000
+    if stair == N and cnt <= 3:
+        return S[N]
+    
+    if (stair,cnt) in dp_table:
+        return dp_table[(stair,cnt)]
+    else:
+        dp_table[(stair,cnt)] = max(dp(stair+1,cnt+1),dp(stair+2, 1)) + S[stair]
+        return dp_table[(stair,cnt)]
+print(dp(0,0))
