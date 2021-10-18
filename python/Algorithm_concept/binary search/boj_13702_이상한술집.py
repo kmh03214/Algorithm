7ff0863bd22334a982849540674c59fe5bbcdff2
MyeongHwan <kmh03214@gmail.com>
2021-10-19 00:14:09 +0900

    이상한술집

---
+++ python/Algorithm_concept/binary search/boj_13702_이상한술집.py
@@ -0,0 +1,16 @@
+import sys
+read = sys.stdin.readline
+N,K = map(int,read().split())
+pots = [int(read()) for i in range(N)]
+low, high = 1, max(pots)+1
+mid = float('inf')
+
+while low+1 < high:
+    mid = (low+high)//2
+    s = sum(p//mid for p in pots)    
+    if s < K:
+        high = mid
+    else:
+        low = mid
+
+print(mid)
