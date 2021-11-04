959167fda9279de07b443484cde679def35e24b1
MyeongHwan <kmh03214@gmail.com>
2021-11-05 01:34:20 +0900

    퀵소트 제자리정렬 추가

--- python/Algorithm_concept/sorting/quicksort_제자리.py
+++ python/Algorithm_concept/sorting/quicksort_제자리.py
@@ -3,42 +3,8 @@ sys.setrecursionlimit(50000)
 read = sys.stdin.readline
 N = int(read())
 arr = [int(read()) for i in range(N)]
-# arr = [10,5,25,8,91,50,20,2,1,21]
-# (10), 5, 1 8 2 50 20 91 25 21
-# 2 5 1 8 (10) 50 20 91 25 21
 
-# def quick_sort(arr):
-#     if len(arr) <= 1:
-#         return arr
-#     left = []
-#     right = []
-#     pv, p = arr[0], 0
-#     for i in range(1,len(arr)):
-#         if pv > arr[i]:
-#             left.append(arr[i])
-#         else:
-#             right.append(arr[i])
-#     return quick_sort(left) + [pv] + quick_sort(right)
-
-# def partition(s,e,p):
-#     while s < e:
-#         while s < e and arr[s] <= arr[p]:
-#             s += 1
-#         while s < e and arr[e] >= arr[p]:
-#             e -= 1
-#         arr[s],arr[e] = arr[e],arr[s]
-#         s+=1
-#         e-=1
-#     arr[e],arr[p] = arr[p],arr[e]
-#     return e # next_pivot
-
-# def quick_sort(l,r,pivot):
-#     if l > r :
-#         return
-#     p = partition(l,r,pivot)
-    
-#     quick_sort(l, p-1 ,l) # (왼) p 오
-#     quick_sort(p+1, r ,p+1) #  왼 p (오)
+# 제자리 퀵소트
 def quick_sort(array, start, end):
     if start >= end: # 원소가 1개인 경우 종료
         return
